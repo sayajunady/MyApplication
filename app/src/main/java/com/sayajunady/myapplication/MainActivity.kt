f91376a3f211442bdcd123c495a1e908c11364a1
Kopi Kode <junadhi@sar.ac.id>
2022-03-23 10:58:40 +0700

    Penambahan Button

--- app/src/main/java/com/sayajunady/myapplication/MainActivity.kt
+++ app/src/main/java/com/sayajunady/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.sayajunady.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSatu: Button = findViewById(R.id.btn_1)
+        btnSatu.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
